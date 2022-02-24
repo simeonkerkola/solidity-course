4eed464bc853211a1d10665c5f9e5f897485d20b
sssmi <simeon.kerkola@gmail.com>
2022-02-24 21:42:57 +0200

    Bytes and strings

---
+++ BytesAndStrings.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.0;
+
+contract BytesAndStrings {
+    bytes public b1 = 'abc';
+    string public s1 = 'abc';
+
+    function addElement() public {
+        b1.push('x');
+
+        // Can't push to strings
+        // s1.push('x')
+    }
+
+    function getElement(uint i) public view returns(bytes1) {
+        return b1[i];
+    }
+
+    function getLength() public view returns(uint) {
+        return b1.length;
+    }s
+}
