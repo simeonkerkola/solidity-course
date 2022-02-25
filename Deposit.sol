1850f2fb791bc49d33cb7a46f6842236bec424f4
sssmi <simeon.kerkola@gmail.com>
2022-02-25 21:40:51 +0200

    Only owner can send

--- Deposit.sol
+++ Deposit.sol
@@ -3,6 +3,12 @@
 pragma solidity 0.8.12;
 
 contract Deposit {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
     // declaring the receive() function that is executed when sending ETH to the contract address
     // it was introduced in Solidity 0.6 and a contract can have only one receive function, 
     // declared with this syntax (without the function keyword and without arguments).
@@ -22,6 +28,9 @@ contract Deposit {
     }
 
     function transferEther(address payable recipient, uint amount) public returns(bool) {
+        // checking that only contract owner can send ether from the contract to another address
+        require(owner == msg.sender, "Transfer failed, you are not the owner!!");
+
         if (amount <= getBalance()) {
             recipient.transfer(amount);
             return true;
